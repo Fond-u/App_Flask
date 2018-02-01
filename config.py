9bcc507dd8d1ba369e07baa2e3b845085a557e22
fond <gregorysallen.e@gmail.com>
2018-02-01 15:07:44 +0100

    modif config

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ import os
 # >>> "".join([random.choice(string.printable) for _ in range(24)])
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
 
-FB_APP_ID = 1556018984483068
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+	basedir = os.path.abspath(os.path.dirname(__file__))
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+	FB_APP_ID = 1556018984483068
+else:
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	FB_APP_ID = 833751096807156
